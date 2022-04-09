8a43c8620cf7dceb7cdcf69107ec1eda3b860af3
Sara Isa <saraisamo123@gmail>
2022-04-10 01:53:14 +0300

    more css

--- client/src/components/home/section1.js
+++ client/src/components/home/section1.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Section1() {
   return (
-    <div>
-      <div>
-        <h1 className="text-2xl font-bold font-serif  ">Welcome to devZoid online shops</h1>
-        <h3>chech out hounders of products posted by devZoid online shop owners.</h3>
-        <di>
-          <button>start browsing</button>
-        </di>
+    <div className="py-20">
+      <div className="m-auto text-center">
+        <h1 className="text-3xl font-bold font-serif py-3">Welcome to devZoid online shops</h1>
+        <h3 className="font-sans">chech out hounders of products posted by devZoid online shop owners.</h3>
+        <div className="py-3">
+          <button className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:bg-blue-color1 font-semibold hover:text-white rounded border border-blue-color1 text-blue-color1 px-6 py-2 text-xs">start browsing</button>
+        </div>
       </div>
     </div>
   )
--- client/src/components/home/section4.js
+++ client/src/components/home/section4.js
@@ -3,13 +3,22 @@ import React from 'react'
 function Section4() {
   return (
     <div>
-        <div>
-          <img src='./ee.jpg' alt='img'/>
-        <h4>name</h4>
-        <h6>the person's testimonial about the website, and they think about it in general.</h6>
-        <span>
-            {/* add react star icons */}
-        </span>
+      <div className="flex flex-wrap sm:flex-no-wrap items-center justify-between  w-4/5 m-auto py-20">
+            <div className="w-full px-2 sm:w-1/3 h-20 rounded-t sm:rounded-l sm:rounded-t-none shadow">
+            <img src='./ee.jpg' alt='img' className="h-20 m-auto w-20 rounded-full"/>
+        <h4 className="font-serif font-semibold text-center text-lg py-5">name</h4>
+        <h6 className="text-center font-sans font-medium">the person's testimonial about the website, and they think about it in general.</h6>
+            </div>
+            <div className="w-full px-2 sm:w-1/3 h-20 shadow ">
+            <img src='./ee.jpg' alt='img' className="h-20 m-auto w-20 rounded-full"/>
+        <h4 className="font-serif font-semibold text-center text-lg py-5">name</h4>
+        <h6 className="text-center font-sans font-medium">the person's testimonial about the website, and they think about it in general.</h6>
+            </div>
+            <div className="w-full px-2 sm:w-1/3 h-20 rounded-b sm:rounded-b-none shadow ">
+            <img src='./ee.jpg' alt='img' className="h-20 m-auto w-20 rounded-full"/>
+        <h4 className="font-serif font-semibold text-center text-lg py-5">name</h4>
+        <h6 className="text-center font-sans font-medium">the person's testimonial about the website, and they think about it in general.</h6>
+            </div>
         </div>
     </div>
   )
